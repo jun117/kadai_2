0ba62ef08821eedcafc0d4a7d3cf9d11f993dca5
jun117 <kbokro@icloud.com>
2022-02-02 22:41:25 +0900

    一覧出力

--- crm_app.py
+++ crm_app.py
@@ -5,13 +5,6 @@ import psycopg2
 
 load_dotenv()
 
-dsn = os.environ.get('SECRET')
-conn = psycopg2.connect(dsn)
-cur = conn.cursor()
-
-with open('welcome.txt', encoding="utf-8") as f:
-    print(f)
-
 """
 要件
 　　・ユーザー一覧表示
@@ -20,3 +13,73 @@ with open('welcome.txt', encoding="utf-8") as f:
 初めに、Welcome to CRM APPlicationの表示をする
 
 """
+
+
+def init_db():
+    # DBに情報を取得　
+    dsn = os.environ.get('DATABASE_URL')
+    # DBに接続　　
+    conn = psycopg2.connect(dsn)
+    cur = conn.cursor()
+    # SQLを用意　
+    with open('schema.sql', encoding="utf-8") as f:
+        sql = f.read()
+        cur.execute(sql)
+    # 実行状態を保存　
+    conn.commit()
+    # コネクションを閉じる
+    conn.close()
+
+
+def register_user(name, age):
+    dsn = os.environ.get('DATABASE_URL')
+
+    conn = psycopg2.connect(dsn)
+    cur = conn.cursor()
+
+    sql = f"INSERT INTO users (name,age)VALUES('{name}',{age})"
+    cur.execute(sql)
+    conn.commit()
+
+    conn.close()
+
+
+def all_users():
+    dsn = os.environ.get('DATABASE_URL')
+    conn = psycopg2.connect(dsn)
+    cur = conn.cursor()
+    # すべてのユーザー情報を取得
+    sql = "SELECT * FROM users;"
+    cur.execute(sql)
+    users = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return users
+
+
+def main():
+    init_db()
+    users = all_users()
+    command = input('Your command>')
+    if command == 'S':
+        print(f"Name:{users[0][0]} Age:{users[0][1]}")
+        print(f"Name:{users[1][0]} Age:{users[1][1]}")
+        print(f"Name:{users[2][0]} Age:{users[2][1]}")
+
+
+"""
+    
+        d = open("schema.sql", "r")
+        print(d.read())
+        d.close()
+"""
+
+"""
+    name = 'Bob'
+    age = 20
+
+    register_user(name,age)
+"""
+
+if __name__ == '__main__':
+    main()
---
+++ welcom_print.py
@@ -0,0 +1,3 @@
+d = open("welcome.txt", "r")
+print(d.read())
+d.close()
